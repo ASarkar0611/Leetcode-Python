f0db90550ee740ab76132758c4a65b99999e828f
ASarkar0611 <arunitasarkar18@gmail.com>
2023-03-16 21:47:27 -0400

    Update Leetcode_Python_1.py

--- Leetcode_Python_1.py
+++ Leetcode_Python_1.py
@@ -56,3 +56,30 @@ sn = Solution()
 s = "aab"
 res = sn.lengthOfLongestSubstring(s)
 #-------------------------------------------------------------------------------------------------------------------------------
+# Medium
+# Given a string s, return the longest palindromic substring in s.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        sbk = s 
+        pal = ''
+        nsl = 0
+        ns = ''
+        for i in range(len(sbk)):
+            nsbk = sbk[i:]
+            pal = ''
+            #print("LOOPING through", nsbk, "/n")
+            for ch in nsbk:
+                #print("ch in nsbk", ch, "/n")
+                pal = pal + ch
+                #print("pal =", pal)
+                if len(pal) > nsl and pal == pal[::-1]:
+                    print("pal - ", pal, "and rpal -", pal[::-1])
+                    nsl = len(pal)
+                    ns = pal
+        #print("Final result", ns)
+        return ns
+        
+sn = Solution()
+s = "babad"
+res = sn.longestPalindrome(s)
