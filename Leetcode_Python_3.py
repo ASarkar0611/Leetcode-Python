5661fb13a4d30ab82ef3aaa5aabe5b6d68f43d6c
ASarkar0611 <arunitasarkar18@gmail.com>
2023-03-24 00:38:46 -0400

    Update Leetcode_Python_3.py

--- Leetcode_Python_3.py
+++ Leetcode_Python_3.py
@@ -73,3 +73,23 @@ sn = Solution()
 nums = [2,2,1]
 res = sn.singleNumber(nums)
 print(res)
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Easy
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+# it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ns = ''.join(c for c in s if c.isalnum()).lower()
+        if ns == ns[::-1]:
+            return True
+        else:
+            return False
+
+
+sn = Solution()
+s = " "
+res = sn.isPalindrome(s)
+print(res)
